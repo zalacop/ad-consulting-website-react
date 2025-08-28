acc46387f9348a1c13f869c7c87f135f92809082
zalacop <103320914+zalacop@users.noreply.github.com>
2025-08-28 15:09:09 +0200

    start with nav bar

---
+++ src/components/Nav/index.tsx
@@ -0,0 +1,15 @@
+function Nav() {
+    return (
+        <>
+            <div>
+                <ul>
+                    <li>About</li>
+                    <li>Service</li>
+                    <li>Portfolio</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Nav;
