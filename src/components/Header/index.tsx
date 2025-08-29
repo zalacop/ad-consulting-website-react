3b6d27db4853cd1dacae219d29e150314ad329fb
zalacop <103320914+zalacop@users.noreply.github.com>
2025-08-29 15:15:11 +0200

    adds padding

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import Nav from "../Nav";
 
 function Header() {
     return (
-        <header className="m-0 bg-blue-500">
+        <header className="m-0 bg-blue-500 py-1">
             <div className="flex justify-between my-6">
                 <h1 className="text-2xl font-bold ml-4 uppercase">AD Consulting</h1>
                 <Nav />
