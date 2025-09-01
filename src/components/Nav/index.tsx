0324a7d8115f47e4c595191483b926370f41f26f
zalacop <103320914+zalacop@users.noreply.github.com>
2025-09-01 14:56:40 +0200

    start on toggle menu

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 function Nav() {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
+       <>
+       <button
+        onClick={toggleMenu}
+        aria-controls="navbar-default"
+        aria-expanded={isMenuOpen}
+       >
+        <span className="sr-only">Toggle Menu</span>
+        <svg
+            aria-hidden="true"
+            fill="none"
+            viewBox="0 0 17 14"
+        >
+            <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+            />
+        </svg>
+       </button>
         <div className="my-auto">
             <ul className="flex">
                 <li className="ml-4 uppercase">About</li>
@@ -7,6 +37,7 @@ function Nav() {
                 <li className="mx-4 uppercase">Portfolio</li>
             </ul>
         </div>
+       </>
     )
 }
 
