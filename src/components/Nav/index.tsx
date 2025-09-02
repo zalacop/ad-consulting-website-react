e30eef729016201f88fb5adf5a9d8f60e5bc5caa
zalacop <103320914+zalacop@users.noreply.github.com>
2025-09-02 15:25:32 +0200

    more styling of hamburger menu

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -12,11 +12,13 @@ function Nav() {
        <>
        <button
         onClick={toggleMenu}
+        className="text-gray-500 dark:text-gray-400 dark:hover:bg-gray-700 inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm md:hidden mr-5"
         aria-controls="navbar-default"
         aria-expanded={isMenuOpen}
        >
         <span className="sr-only">Toggle Menu</span>
         <svg
+            className="h-5 w-5"
             aria-hidden="true"
             fill="none"
             viewBox="0 0 17 14"
@@ -30,8 +32,10 @@ function Nav() {
             />
         </svg>
        </button>
-        <div className="my-auto">
-            <ul className="flex">
+        <div 
+            className={`w-full md:block md:w-auto ${isMenuOpen ? "" : "hidden"}`}
+            id="navbar-default">
+            <ul className="mt-4 flex flex-col items-center p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:p-0 rtl:space-x-reverse">
                 <li className="ml-4 uppercase">About</li>
                 <li className="ml-4 uppercase">Service</li>
                 <li className="mx-4 uppercase">Portfolio</li>
